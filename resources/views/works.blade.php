49b2960812a618cbcbda43edfba36a0cb0e2419d
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2024-03-15 14:33:51 -0400

    improved Work page layout

--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -8,15 +8,65 @@
         My Works
       </span>
     </div>
-    @if (count($all_projects) > 0)
-      <div class="projectScroll">
-        @foreach ($all_projects as $one_project)
-          <div class="oneProject projectImg" style='background-image:url({{ url($one_project->img_path) }})'>
-            {{ $one_project->name }}
-          </div>
-        @endforeach
-      </div>
-    @endif
+    <div class="workGallery">
+      @if (count($all_projects) > 0)
+        <div class="projectList">
+          @foreach ($all_projects as $one_project)
+            <svg
+              class="oneProject"
+              data-id="{{ $one_project->id }}"
+              viewBox="0 0 100 100"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink">
+              <defs>
+                <pattern 
+                  id="projectSquare_{{ $one_project->id }}" 
+                  patternUnits="userSpaceOnUse" 
+                  width="85" 
+                  height="85">
+                    <image 
+                      class="projectImg"
+                      href="{{ url($one_project->img_path) }}" 
+                      x="15" 
+                      y="15" 
+                      width="70" 
+                      height="70" />
+                </pattern>
+                <radialGradient 
+                  id="projectCircle_{{ $one_project->id }}">
+                    <stop offset="97%" stop-color="rgba(11, 45, 57, 0.7)"/>
+                    <stop offset="97%" stop-color="#E7B85A"/>
+                </radialGradient>
+              </defs>
+              <a href="{{ $one_project->site_url }}" target="_blank">
+                <circle 
+                  fill="url('#projectCircle_{{ $one_project->id }}')"
+                  cx="50" 
+                  cy="50" 
+                  r="50" />
+              </a>
+              <a href="{{ $one_project->site_url }}" target="_blank">
+                <rect 
+                  x="15" 
+                  y="15" 
+                  height="70" 
+                  width="70" 
+                  fill="url('#projectSquare_{{ $one_project->id }}')" />
+              </a>
+            </svg>
+          @endforeach
+        </div>
+        <div>
+          <!-- Testing -->
+        </div>
+      @else 
+        <div class="projectList">
+            <div class="oneProject projectImg" style='background-image:url({{ url($one_project->img_path) }})'>
+              <i>No works found</i>
+            </div>
+        </div>
+      @endif
+    </div>
   </div>
 </div>
 @endsection

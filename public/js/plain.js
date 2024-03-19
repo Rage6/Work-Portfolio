35beb6474b24fca7ca06bd0f720443ddc10c8371
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2024-03-19 16:50:56 -0400

    adjust large star init placement

--- public/js/plain.js
+++ public/js/plain.js
@@ -1,8 +1,15 @@
 console.log("This worked");
 
-// Adjusts the 1vh size for mobile devices
+// Adjusts the 1vh and total height sizes for mobile devices
 let vhInteger = window.innerHeight;
 let vh = (vhInteger * 0.01) + "px";
 let fullHeight = vhInteger + "px";
 document.documentElement.style.setProperty('--vh', vh);
-document.documentElement.style.setProperty('height', fullHeight);
+document.documentElement.style.setProperty('--height', fullHeight);
+
+// Adjusts the 1vw and total width sizes for mobile devices
+let vwInteger = window.innerWidth;
+let vw = (vwInteger * 0.01) + "px";
+let fullWidth = vwInteger + "px";
+document.documentElement.style.setProperty('--vw', vw);
+document.documentElement.style.setProperty('--width', fullWidth);

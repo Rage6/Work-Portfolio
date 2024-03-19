135fe6dc64886891f973eee0d2e793a6a95c7f21
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2024-03-19 15:54:52 -0400

    refining html height

--- public/js/plain.js
+++ public/js/plain.js
@@ -1,8 +1,8 @@
 console.log("This worked");
 
 // Adjusts the 1vh size for mobile devices
-let vhInteger = window.innerHeight * 0.01;
-let vh = vhInteger + "px";
-let fullHeight = (vhInteger * 100) + "px";
+let vhInteger = window.innerHeight;
+let vh = (vhInteger * 0.01) + "px";
+let fullHeight = vhInteger + "px";
 document.documentElement.style.setProperty('--vh', vh);
 document.documentElement.style.setProperty('height', fullHeight);

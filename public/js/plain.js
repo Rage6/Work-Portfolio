3edeeb3d4547643cd85543b4a1ee0100688ce4b6
Nicholas Vogt <nicholas.vogt2017@gmail.com>
2024-03-19 15:34:32 -0400

    another test w/o stars

--- public/js/plain.js
+++ public/js/plain.js
@@ -3,4 +3,6 @@ console.log("This worked");
 // Adjusts the 1vh size for mobile devices
 let vhInteger = window.innerHeight * 0.01;
 let vh = vhInteger + "px";
+let fullHeight = (vhInteger * 100) + "px";
 document.documentElement.style.setProperty('--vh', vh);
+document.documentElement.style.setProperty('height', fullHeight);
